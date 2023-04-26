3fc3141203a1b9aba28c569dd9f72a3670387c99
Samantha Chow <sc_chow99@hotmail.com>
2023-04-26 14:05:00 -0700

    adding changes

--- webscraping/src/main/java/org/example/Main.java
+++ webscraping/src/main/java/org/example/Main.java
@@ -2,22 +2,29 @@ package org.example;
 import org.jsoup.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
-
+import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Document doc;
+        String url = "https://pokemondb.net/pokedex/national";
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        //Testing to get header 1 = List of Pokemon
+        Elements header1 = doc.select("h1");
+        //Testing output
+        System.out.println(header1.html());
+
+        //Elements gen1pkmlist = doc.select("div.infocard-list infocard-list-pkmn-lg").select("div.infocard");
+
+        //System.out.println(gen1pkmlist.html());
+
+
+
     }
 }
